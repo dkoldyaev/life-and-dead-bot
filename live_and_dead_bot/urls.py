c607561090dea6b029cb62141f3d08cd241e3d6a
koldyaev <koldyaev@tochka.com>
2021-08-27 23:16:26 +0300

    Включил статику

--- live_and_dead_bot/urls.py
+++ live_and_dead_bot/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     url(r'admin/', admin.site.urls),
     url(r'markdownx/', include('markdownx.urls')),
     url(r'', MessageView.as_view()),
-]# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
