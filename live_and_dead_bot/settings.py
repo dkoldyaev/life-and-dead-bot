78612e4c547fa0017d998a480c2bf9c141b848fa
koldyaev <koldyaev@tochka.com>
2021-08-27 23:38:48 +0300

    строгий url media 2

--- live_and_dead_bot/settings.py
+++ live_and_dead_bot/settings.py
@@ -135,6 +135,6 @@ STATIC_URL = "/static/"
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 django_heroku.settings(locals())
