105775978051002e4d984136bebc6c6276a14c7b
koldyaev <koldyaev@tochka.com>
2021-08-28 16:50:04 +0300

    [e`nbrf]

--- live_and_dead_bot/settings.py
+++ live_and_dead_bot/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'live_and_dead_bot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db3.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
